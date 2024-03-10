6c8de21c4b282907fa729c77aec26f625dda21e8
LucasBegnini10 <begninilucas12@gmail.com>
2024-03-10 13:18:42 -0300

    fix: allowed all origins in cors config

--- src/main/java/com/server/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/server/ecommerce/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("https://ecommerce-web-ten-topaz.vercel.app"));
+                    config.setAllowedOrigins(List.of("*"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
